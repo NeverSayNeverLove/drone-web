e6abdb1fbd97ffd9fccbcd4b1cd761df21c92eac
thuydungnguyen <dung.ntt160740@sis.hust.edu.vn>
2019-03-05 09:36:02 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { UserCalendarComponent } from './user-calendar/user-calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductComponent
+    ProductComponent,
+    UserCalendarComponent
   ],
   imports: [
     BrowserModule,
