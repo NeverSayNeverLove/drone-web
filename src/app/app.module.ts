05c9125166ae4d55d23012d612ef16da2eba006a
thuydungnguyen <dung.ntt160740@sis.hust.edu.vn>
2019-03-05 11:40:46 +0700

    init config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { SigninComponent } from './signin/signin.component';
+// user
 import { UserCalendarComponent } from './user-calendar/user-calendar.component';
+import { HeaderComponent } from './user-general/header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { UserCalendarComponent } from './user-calendar/user-calendar.component';
     HomeComponent,
     ProductComponent,
     SigninComponent,
-    UserCalendarComponent
+    UserCalendarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
