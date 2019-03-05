5401a85bf67de493fbdf58253b89dee65d249c9d
thuydungnguyen <dung.ntt160740@sis.hust.edu.vn>
2019-03-05 09:45:19 +0700

    create signin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductComponent
+    ProductComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
