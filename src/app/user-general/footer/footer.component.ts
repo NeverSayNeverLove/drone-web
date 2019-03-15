d9a908e3c6c5a43538b69951ff502daad0950bf9
thuydungnguyen <dung.ntt160740@sis.hust.edu.vn>
2019-03-15 16:56:51 +0700

    edit header footer

---
+++ src/app/user-general/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItemModel} from '@syncfusion/ej2-angular-navigations';
+
+@Component({
+  selector: 'user-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+}
